ccff197c24103a098a8b32a518817486b81cb98a
Ali Naqvi <alibnaqvi851@gmail.com>
2022-11-20 17:34:27 -0800

    Delete main copy.py

--- main copy.py
+++
@@ -1,8 +0,0 @@
-import json
-
-tweet = []
-
-for line in open("/Users/alinaqvi/Desktop/SWE/yelp_dataset/yelp_academic_dataset_business.json", "r"):
-    tweet.append(json.loads(line))
-
-print(tweet[0])
