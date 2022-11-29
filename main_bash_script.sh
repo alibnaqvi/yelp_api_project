243c3ba57265e9d2c67b4524eceacb6669778c4f
m-yen <101024037+m-yen@users.noreply.github.com>
2022-11-29 13:18:08 -0800

    created branch to start building bash script. Will probably become main branch later

---
+++ main_bash_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir temp
+
+mv yelp.py /temp
+
+mv trip_adv.py /temp
+
+cd temp
+
+python yelp.py
+
+python trip_adv.py
+
+git clone #fill in with ML script
+
+python #fill in with ML script
+
+#something with their output file
+#
+#
+#
+#
+
+mv yelp.py /..
+
+mv trip_adv.py /..
+
+cd /..
+
+rm -rf temp
