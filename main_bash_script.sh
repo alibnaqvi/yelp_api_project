867b4c8c53151a77e298478c2dcdaebdcadb6cd9
m-yen <101024037+m-yen@users.noreply.github.com>
2022-12-01 00:25:16 -0800

    Actually finished everything. Just need to put in columns according to ML

--- main_bash_script.sh
+++ main_bash_script.sh
@@ -4,17 +4,11 @@ mkdir temp
 
 mv yelp.py temp
 
-mv trip_adv.py temp
-
 cd temp
 
 git clone #fill in with ML script
 
 python yelp.py
-
-rm data.csv
-
-python trip_adv.py
 
 echo "Running analysis..."
 
@@ -23,8 +17,6 @@ python #fill in with ML script
 mv results.csv ..
 
 mv yelp.py ..
-
-mv trip_adv.py ..
 
 cd ..
 
